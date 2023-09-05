e73d36fb3812eadf9865afe52cc6e9e215322731
Rüger <Yaron.Rueger@telekom.de>
2023-09-05 12:49:16 +0200

    svelte-version

---
+++ app/src/routes/+page.svelte
@@ -0,0 +1,20 @@
+<script>
+    import Typewriter from 'svelte-typewriter'
+</script>
+
+<main>
+    <Typewriter cursor={true} delay={500}>
+        <h1>Hi, my Name is Yaron Rüger</h1>
+    </Typewriter>
+</main>
+
+<style>
+    :root{
+        background-color: #2C3333;
+        color: #CBE4DE;
+    }
+    main{
+        --cursor-color: white;
+        --cursor-width: 4px;
+    }
+</style>
