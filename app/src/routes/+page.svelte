168d40150608b91d6db3e5ccf275178776ec836f
Rüger <Yaron.Rueger@telekom.de>
2023-09-07 11:18:00 +0200

    iv1

--- app/src/routes/+page.svelte
+++ app/src/routes/+page.svelte
@@ -1,23 +1,44 @@
 <script>
     import Typewriter from 'svelte-typewriter'
-    import { fade, blur, fly, slide, scale } from "svelte/transition";
+    import { fade, blur, fly, slide, scale } from 'svelte/transition';
+    import { onMount } from 'svelte';
+    import Icon from '@iconify/svelte';
+
+    let visible = false;
+    onMount(()=>{ visible = true; })
     </script>
 
-<main>
+<section class="part1">
     <div class="header">
         <Typewriter cursor={false} delay={500}>
-            <h1>Hi, my Name is Yaron Rüger</h1>
+            <h1>Hi, my Name is <span class="name">Yaron Rüger</span></h1>
         </Typewriter>
         <Typewriter cursor={false} delay={1465}>
             <h2>dual Student of computer science</h2>
         </Typewriter>
-        <div in:fade="{{delay: 2000, duration: 2000}}" class="links">
-            <p>Test</p>
-        </div>
-        <a href="https://github.com"><img  src="../../icons8-github.svg" alt=""></a>
+        {#if visible}
+            <div class="links" in:fade="{{delay: 2500, duration: 2000}}">
+                <ul>
+                    <li class="githubIcon">
+                        <a href="https://github.com">
+                            <img  src="../../githubIcon.svg" alt="">
+                        </a>
+                    </li>
+                    <li class="mailIcon">
+                        <a href="https://github.com">
+                            <img  src="../../mailIcon.svg" alt="">
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        {/if}
     </div>
-    
-</main>
+    {#if visible}
+        <div class="iconDown" in:slide="{{delay: 2760, duration: 500}}">
+            <Icon icon="bx:down-arrow" style="font-size: 3vh; color:#176B87;"/>
+        </div>
+    {/if}
+</section>
 
 <style>
     @font-face {
@@ -26,14 +47,31 @@
         src: url(../../RandyGG/randy-gg.ttf);
         }
     :root{
-        background-color: #2C3333;
-        color: #CBE4DE;
+        background-color: #001C30;
+        color: #DAFFFB;
+    }
+    @keyframes bounce {
+        0%, 20%, 50%, 80%, 100% {
+            transform: translateY(0);
+        }
+        40% {
+            transform: translateY(-20px);
+        }
+        60% {
+            transform: translateY(-15px);
+        }
+    }
+    .part1{
+        min-height: 100vh;
     }
     h1{
         margin: 0;
         padding: 10px;
         font-family: LouisGeorgeCafe;
         font-size:5vh;
+    }
+    .name {
+    color: #64CCC5;
     }
     h2{
         font-family: LouisGeorgeCafe;
@@ -44,7 +82,27 @@
         text-align: center;
         padding-top: 30vh;
     }
-    main{
-        min-height: 100vh;
+    .links{
+        padding-top: 2.5vh;
+    }
+    ul{
+        list-style: none;
+        padding: 0;
+    }
+    li{
+        display: inline;
+    }
+    .githubIcon{
+        padding-right: 2vh;
+    }
+    .mailIcon{
+        padding-left: 2vh;
+    }
+    .iconDown{
+        min-width: 100%;
+        text-align: center;
+        position: absolute;
+        bottom: 1.5vh;
+        animation: bounce 2s infinite;
     }
 </style>
