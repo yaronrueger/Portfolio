b7edd5528f0dc4b16b61deeb599ef1f1ad7d52a2
Yaron Rüger <105042550+yaronrueger@users.noreply.github.com>
2023-11-20 10:32:19 +0100

    Delete portfolio directory

--- portfolio/build/server/chunks/2-abf5ca8d.js
+++
@@ -1,9 +0,0 @@
-const index = 2;
-let component_cache;
-const component = async () => component_cache ??= (await import('./_page.svelte-246cee66.js')).default;
-const imports = ["_app/immutable/nodes/2.a8322911.js","_app/immutable/chunks/scheduler.3a2ff664.js","_app/immutable/chunks/index.d1b0ad94.js","_app/immutable/chunks/preload-helper.a4192956.js"];
-const stylesheets = ["_app/immutable/assets/2.ce3555d7.css"];
-const fonts = [];
-
-export { component, fonts, imports, index, stylesheets };
-//# sourceMappingURL=2-abf5ca8d.js.map
