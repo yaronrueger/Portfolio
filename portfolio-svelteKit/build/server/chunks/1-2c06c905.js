5d832005146b7c1e72e3850243d6e9915cfd5e57
Rüger <Yaron.Rueger@telekom.de>
2023-11-20 11:34:52 +0100

    fix docker try

--- portfolio-svelteKit/build/server/chunks/1-2c06c905.js
+++ portfolio-svelteKit/build/server/chunks/1-2c06c905.js
@@ -1,9 +1,9 @@
 const index = 1;
 let component_cache;
 const component = async () => component_cache ??= (await import('./error.svelte-72c24af7.js')).default;
-const imports = ["_app/immutable/nodes/1.bc2edc2f.js","_app/immutable/chunks/scheduler.3a2ff664.js","_app/immutable/chunks/index.d1b0ad94.js","_app/immutable/chunks/singletons.acbd8810.js"];
+const imports = ["_app/immutable/nodes/1.c5f005e6.js","_app/immutable/chunks/scheduler.3a2ff664.js","_app/immutable/chunks/index.d1b0ad94.js","_app/immutable/chunks/singletons.19be5892.js"];
 const stylesheets = [];
 const fonts = [];
 
 export { component, fonts, imports, index, stylesheets };
-//# sourceMappingURL=1-121ae8b1.js.map
+//# sourceMappingURL=1-2c06c905.js.map
--- portfolio-svelteKit/build/server/manifest.js
+++ portfolio-svelteKit/build/server/manifest.js
@@ -10,10 +10,10 @@ return {
 	assets: new Set(["assets/particles.json","fonts/LouisGeorgeCafe/Louis George Cafe Bold Italic.ttf","fonts/LouisGeorgeCafe/Louis George Cafe Italic.ttf","fonts/LouisGeorgeCafe/Louis George Cafe Light Italic.ttf","fonts/LouisGeorgeCafe/LouisGeorgeCafe.ttf","fonts/LouisGeorgeCafe/LouisGeorgeCafeBold.ttf","fonts/LouisGeorgeCafe/LouisGeorgeCafeLight.ttf","fonts/RandyGG/randy-gg-bold.ttf","fonts/RandyGG/randy-gg.ttf","images/favicon.png","images/favicon.svg"]),
 	mimeTypes: {".json":"application/json",".ttf":"font/ttf",".png":"image/png",".svg":"image/svg+xml"},
 	_: {
-		client: {"start":"_app/immutable/entry/start.7efc8224.js","app":"_app/immutable/entry/app.a93994e6.js","imports":["_app/immutable/entry/start.7efc8224.js","_app/immutable/chunks/scheduler.3a2ff664.js","_app/immutable/chunks/singletons.acbd8810.js","_app/immutable/entry/app.a93994e6.js","_app/immutable/chunks/preload-helper.a4192956.js","_app/immutable/chunks/scheduler.3a2ff664.js","_app/immutable/chunks/index.d1b0ad94.js"],"stylesheets":[],"fonts":[]},
+		client: {"start":"_app/immutable/entry/start.136bcd7c.js","app":"_app/immutable/entry/app.305dd49b.js","imports":["_app/immutable/entry/start.136bcd7c.js","_app/immutable/chunks/scheduler.3a2ff664.js","_app/immutable/chunks/singletons.19be5892.js","_app/immutable/entry/app.305dd49b.js","_app/immutable/chunks/preload-helper.a4192956.js","_app/immutable/chunks/scheduler.3a2ff664.js","_app/immutable/chunks/index.d1b0ad94.js"],"stylesheets":[],"fonts":[]},
 		nodes: [
 			__memo(() => import('./chunks/0-7c2b3153.js')),
-			__memo(() => import('./chunks/1-121ae8b1.js')),
+			__memo(() => import('./chunks/1-2c06c905.js')),
 			__memo(() => import('./chunks/2-65354aea.js'))
 		],
 		routes: [
